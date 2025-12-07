c934f7ab7f0fc969c6b6ef9df0e015d49d58c971
KeatonShawhan <64229449+KeatonShawhan@users.noreply.github.com>
2025-12-06 16:34:48 -0800

    trying repo manager locally

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 import asyncio
 from agents.calculator import CalculatorAgent
 from utils.repoManager import RepoManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 async def main():
     """Run calculator agent with sample task"""
-    RepoManager.
+    repoManager = RepoManager(
+        cache_dir="./repo_cache"
+    )
+
+    # Move this to clone in the sandbox for the agent to use, rather than have it look locally
+    repoManager.ensure_repo_updated(os.getenv("REPO_URL"), "main", os.getenv("GITHUB_TOKEN"))
+
     # Create and use calculator agent
     async with CalculatorAgent() as agent:
         # Example task: read file and perform calculation
         task = (
-            "Multiply 6 and 7 and then return the result to me. Only return the number."
+            "Read me the files and folders in the sandbox. Dont look at any files on my local machine."
         )
         
         await agent.execute_task(task)
