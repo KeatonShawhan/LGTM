bb3b81bc38a87ce68ee8c59106a6d8cc7639e3e9
Sebewe <38896608+Sebewe@users.noreply.github.com>
2025-12-12 05:23:07 -0800

    outline for composer json output

--- utils/repoComposer.py
+++ utils/repoComposer.py
@@ -21,3 +21,28 @@ def compose_file(file_path: os.PathLike, queryAgent: QueryAgent):
     assert os.path.exists(file_path), f"File path does not exist and failed to compose: {file_path}"
     assert os.path.isfile(file_path), f"Provided directory and *NOT* filepath: {file_path}"
 
+"""
+
+json format
+
+{
+
+path: os.PathLike -> path to the given file
+isfile: bool -> True if file, False if directory
+subfiles: list[os.PathLike] -> empty if file, contains subfiles (including root directory) if directory
+description: str: if file, describes the function/activity of the file (general overview of it's functions and what it performs)
+                  if directory, summary will contain an overview of what the sub-listed files do. (TODO: figure this out)
+}
+functions: list[dict] -> (empty if directory) 
+    { each function is described as
+        arguments: list[str] -> in format "argument: type"
+        return_type: str
+        description: str -> through a call to claude, the function is described in 1-3 sentences (brevity strongly preferred)
+    }
+
+imports: list[str] -> lists the general imports to the file, empty if directory TODO: list or str?
+non_function_code: str -> contains all general code not included in functions TODO: how important is this? should we do more?
+
+The general intent of this is to reduce the token input to the model, and to reduce the amount of comprehension
+we rely on the model to do. Less is more? 
+"""
