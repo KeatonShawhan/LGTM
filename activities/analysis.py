bd7b37544f0d5ceefa74326f95d2eccb8ee4c655
“Keaton <kshawhan@ucsc.edu>
2025-12-15 12:51:43 -0800

    repo contexter - probably will get removed

--- activities/analysis.py
+++ activities/analysis.py
@@ -3,23 +3,23 @@ import re
 from temporalio import activity
 import json
 from agents.codeAnalysis import CodeAnalysisAgent
-
+from utils.repoContexter import generate_repo_context
 
 @activity.defn
 async def summarize_repo_activity(repo_path: str) -> dict:
-    summary = {"repo_path": repo_path}
     # Pass summary to agent
-    async with CodeAnalysisAgent(
-        directories=[repo_path] 
-    ) as agent:
-        task = f"""
-        You are provided with a code repository located at: {repo_path}
+    summary = generate_repo_context(repo_path)
 
-        Your task is to return me 5 file names.
-        """
 
-        llm_response = await agent.execute_task(task)
+    # async with CodeAnalysisAgent() as agent:
+    #     task = f"""
+    #     You are provided with a code repository located at: {repo_path}
+
+    #     Your task is to return me 5 file names.
+    #     """
+
+    #     llm_response = await agent.execute_task(task)
 
     return {
-        "llm_summary": llm_response
+        "llm_summary": summary
     }
--- test.py
+++ test.py
@@ -35,15 +35,15 @@ async def main():
         print(f"Finished workflow to clone repo")
         print(environment)
         
-        # analysis = await client.start_workflow(
-        #     CodeAnalysisWorkflow.run,
-        #     args=[environment["repo_path"], "standard", None],
-        #     id=f"code-analysis-{asyncio.get_event_loop().time()}",
-        #     task_queue="code-dev-queue",
-        #     retry_policy=RetryPolicy(maximum_attempts=2)
-        # )
+        analysis = await client.start_workflow(
+            CodeAnalysisWorkflow.run,
+            args=[environment["repo_path"], "standard", None],
+            id=f"code-analysis-{asyncio.get_event_loop().time()}",
+            task_queue="code-dev-queue",
+            retry_policy=RetryPolicy(maximum_attempts=2)
+        )
 
-        # print(f"Started workflow to analyze code: {analysis.id}")
+        print(f"Started workflow to analyze code: {analysis.id}")
 
         # Keep worker running
         await asyncio.Event().wait()
